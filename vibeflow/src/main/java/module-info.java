758d1499e6687ffbfc716c55db852ac46f9283a7
Luigii_11 <luigi11autorino@gmail.com>
2026-05-30 16:46:16 +0200

    Setup iniziale: configurazione Maven con Java 23.0.2, JavaFX e Gson

---
+++ vibeflow/src/main/java/module-info.java
@@ -0,0 +1,7 @@
+module it.unisa.diem.sad_gruppo6 {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    opens it.unisa.diem.sad_gruppo6 to javafx.fxml;
+    exports it.unisa.diem.sad_gruppo6;
+}
